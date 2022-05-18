4aa672b3ec80d00fc74cf135d48c200ad2acb802
Bruce07gt <94225931+Bruce07gt@users.noreply.github.com>
2022-05-18 09:39:56 -0300

    Add files via upload

---
+++ Par&impar/src/NumImpar.java
@@ -0,0 +1,15 @@
+public class NumImpar {
+
+	public static void main(String[] argds) {
+		
+		int numero = 8;
+		int parOuImpar = numero %2;
+		
+		if(parOuImpar == 0) {
+			System.out.println("Numero é Par");
+		} else {
+			System.out.println("Numero é Impar");
+		}
+		
+	}
+}
